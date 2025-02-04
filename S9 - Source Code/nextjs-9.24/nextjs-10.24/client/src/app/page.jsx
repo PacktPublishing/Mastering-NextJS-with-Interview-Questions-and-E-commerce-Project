f124ca2bf8188c32308401f68c706bcc3f9ec3a9
Kavyashree Shah <40597017+kavyashreeshah@users.noreply.github.com>
2025-02-04 15:45:04 +0530

    Added

---
+++ S9 - Source Code/nextjs-9.24/nextjs-10.24/client/src/app/page.jsx
@@ -0,0 +1,22 @@
+import { getProducts, getProductTypes } from "@/actions/productActions";
+import HomeScreen from "@/screens/home";
+
+export default async function Home({searchParams}) {
+  const products = await getProducts(searchParams);
+  const productTypesRes = await getProductTypes();
+  const productTypes = [
+    {label:"All", value:"all"},
+    ...productTypesRes?.data?.map((item)=>({
+      label: item.name,
+      value: item.id
+    }))
+  ]
+
+  return (
+    <HomeScreen 
+      searchParams={searchParams} 
+      products={products}
+      productTypes={productTypes}
+    />
+  );
+}
---
+++ S9 - Source Code/nextjs-9.24/nextjs-10.24/client/src/screens/home/ProductCard.jsx
@@ -0,0 +1,64 @@
+import { StarIcon } from "@/components/icons";
+import Button from "@/components/ui/Button";
+import Image from "next/image";
+
+
+const ProductCard = ({product})=>{
+    const BASE_URL = process.env.BASE_URL;
+
+    return(
+        <div 
+            className="bg-white rounded-xl shadow-lg w-full h-full min-h-[624px]"
+            key={product?.id}    
+        >
+            <Image
+                className="w-full h-full rounded-t-xl max-h-96 object-cover"
+                src={`${BASE_URL}${product?.image}`}
+                alt="product"
+                width={0}
+                height={0}
+                sizes="100vw"
+            />
+            <div className="p-5 space-y-4">
+                <div className="space-y-1">
+                    <span className="text-2xl font-semibold leading-5">{product?.name}</span>
+                    <p className="text-gray-400 text-md truncate">{product?.description}</p>
+                </div>
+
+                <div className="space-y-0">
+                    <div className="flex justify-between items-center">
+                        <div className="flex gap-x-3 items-center text-xl font-semibold">
+                            <span className="text-gray-500 line-through">${product?.mrp}</span>
+                            <span className="text-2xl">${product?.sellPrice}</span>
+                        </div>
+                        <span className="text-gray-400 text-md">{product?.currentStock} left</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div className="flex gap-x-1">
+                            {
+                                [...Array(product?.rating || 0)].map((star)=>(
+                                    <StarIcon key={star}/>
+                                ))
+                            }
+                        </div>
+                        <div className="product-type-label">
+                            {product?.productType?.name}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex gap-x-2">
+                    <Button className="w-full custom-outline-btn">
+                        Add to Cart
+                    </Button>
+                    <Button className="w-full">
+                        Buy Now
+                    </Button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard;
