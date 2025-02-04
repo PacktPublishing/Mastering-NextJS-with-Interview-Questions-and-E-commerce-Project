672fd35d00f21fa5c606ebae771266b95babbd8c
Kavyashree Shah <40597017+kavyashreeshah@users.noreply.github.com>
2025-02-04 15:36:08 +0530

    Updated

--- S9 - Source Code/nextjs-9.24/nextjs-10.24/admin/src/app/(withLayout)/page.js
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return (
-    <h1>This is the Admin Section.</h1>
-  );
-}
--- S9 - Source Code/nextjs-9.24/nextjs-10.24/admin/src/lib/utils.js
+++
@@ -1,7 +0,0 @@
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
-
-
-export function cn(...classNames){
-    return twMerge(clsx(classNames));
-}
